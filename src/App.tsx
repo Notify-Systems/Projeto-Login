16d4e93f6e2ab7880898a6f7b73d6e3cc2e73bc4
Lucas <lcorte110210@gmail.com>
2026-01-15 22:27:26 -0300

    Primeira aplicação de teste para o contexto de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
+  const context = useContext(ThemeContext);
+  const theme = context?.theme;
+  const actualTheme = theme === "auto" ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light") : theme;
+  
   return (
     <>
-      <div className="w-screen h-screen"> {/*largura e altura da tela definidas numa div*/}
+      <div className={`w-screen h-screen bg-[image:var(--${actualTheme}-bg)]`}> {/*largura e altura da tela definidas numa div*/}
         <Outlet /> {/*tudo que aparece na tela*/}
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Routes, Route } from "react-router-dom";
 import './index.css'
-import { ThemeProvider } from './routes/ThemeContext.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx';
 import App from './App.tsx'
 import Login from "./pages/Login";
 
